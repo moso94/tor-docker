10a2fb68fe2fd5a7e2315c96167b9d16ae0d8b72
Mohammad Sobhani <mo.sobhani73@gmail.com>
2020-06-03 04:17:21 +0430

    wait bug fix

--- wait-for.sh
+++ wait-for.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-while [ $(curl --socks5 localhost:9150 --socks5-hostname localhost:9150 -s -o /dev/null -w "%{http_code}" https://check.torproject.org/) != 200 ]
+while [ $(curl --socks5 tor:9150 --socks5-hostname tor:9150 -s -o /dev/null -w "%{http_code}" https://check.torproject.org/) != 200 ]
 do
     sleep 5
 done
